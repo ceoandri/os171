40bb72d4b5bd6e1e4486edcdb7e4532f0a78293c
Andri Nur Rochman - 1506688986 <andri.nur51@badak.cs.ui.ac.id>
2017-02-16 22:24:47 +0700

    add log

--- practice/coba1.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-for VAR in Satu Dua Tiga Empat
-do
-	echo $VAR
-done
-exit 0
--- practice/coba2.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-echo "Ini PID[$$] dengan $# ARGUMEN YAITU:"
-echo "(satu list) $*"
-for VAR in "$*" ; do echo $VAR; done
-
-echo "(satu per satu) $@"
-for VAR in "$@"; do echo $VAR; done
-
-exit 0
